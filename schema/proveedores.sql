935aa8dd004fe62ade05dddbe0ea08178e92a486
matiasperlo <103325934+matiasperlo@users.noreply.github.com>
2023-10-29 15:18:05 -0300

    actualizacion proveedores

--- schema/proveedores.sql
+++ schema/proveedores.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS "proveedores"(
     "id" INTEGER NOT NULL,
     "razon_social" TEXT NOT NULL,
-    "contacto_fisico" TEXT,
+    "persona_de_contacto" TEXT,
     "email" TEXT,
     "telefono" TEXT,
     "direccion" TEXT,
     "fecha_registro" INTEGER NOT NULL,
-    "fecha_alta" INTEGER,
-    "fecha_baja" INTEGER,
+    "fecha_modificacion" INTEGER,
+    "activo" INTEGER,
     CONSTRAINT "PK_proveedores" PRIMARY KEY("id" AUTOINCREMENT)
 );
