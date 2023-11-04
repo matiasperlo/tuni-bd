aed35b5ca9100bda94024c6568406dff6002be4a
matiasperlo <103325934+matiasperlo@users.noreply.github.com>
2023-11-04 15:22:29 -0300

    correccion tabla articulo, creacion de maquinas de estado en sinvas

--- schema/articulos.sql
+++ schema/articulos.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS "articulos" (
 	"descripcion" TEXT,
 	"fecha_registro" INTEGER NOT NULL,
 	"fecha_alta" INTEGER,
-	"fecha_baja" INTEGER,
+	"fecha_modificacion" INTEGER,
+	"activo" INTEGER,
 	"foto_principal" TEXT,
 	"min_hombre" INTEGER,
 	CONSTRAINT "PK_articulos" PRIMARY KEY("id" AUTOINCREMENT)
