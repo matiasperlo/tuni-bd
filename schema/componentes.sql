886dabb2967ce0c62456df48dcb415c3aad38c89
matiasperlo <103325934+matiasperlo@users.noreply.github.com>
2023-11-04 11:57:28 -0300

    actualizacion componentes

--- schema/componentes.sql
+++ schema/componentes.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS "componentes" (
 	"nombre" TEXT NOT NULL,
 	"descripcion" TEXT,
 	"fecha_registro" INTEGER,
-	"fecha_baja" INTEGER,
+	"fecha_modificacion" INTEGER,
+	"activo" INTEGER,
 	"id_textura" INTEGER,
 	CONSTRAINT "PK_componentes" PRIMARY KEY("id" AUTOINCREMENT),
 	CONSTRAINT "FK_texturas__componentes" FOREIGN KEY("id_textura") REFERENCES "texturas"("id")
