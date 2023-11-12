2c95cac4442099b1d7c0bfa97ece1217e872818f
Albano Zupichiatti <89983599+Albano21@users.noreply.github.com>
2023-11-12 18:45:08 -0300

    actualizacion de fk en precios_articulo_y_componen

--- schema/precios_articulo_Y_componentes.sql
+++ schema/precios_articulo_Y_componentes.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS "precios_articulo_Y_componentes" (
 	"fecha" INTEGER NOT NULL,
 	"id_precio_componente" INTEGER NOT NULL,
 	CONSTRAINT "PK_precios_articulo_Y_componentes" PRIMARY KEY("id_articulo", "fecha", "id_precio_componente"),
-    CONSTRAINT "FK_id_articulo" FOREIGN KEY("id_articulo") REFERENCES "articulos"("id"),
+    CONSTRAINT "FK_id_articulo" FOREIGN KEY("id_articulo", "fecha") REFERENCES "precios_articulo"("id_articulo", "fecha"),
     CONSTRAINT "FK_id_precio_componente" FOREIGN KEY("id_precio_componente") REFERENCES "precios_componente"("id")
 );
